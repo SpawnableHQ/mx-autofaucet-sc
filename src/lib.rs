19d52fe7fbcaef3d78924d466103f97d7f0fdc27
Micha Vie <micha@vleap.io>
2024-05-14 23:39:38 +0200

    add admins

--- src/lib.rs
+++ src/lib.rs
@@ -10,4 +10,26 @@ pub trait FaucetContract {
 
     #[upgrade]
     fn upgrade(&self) {}
+
+    #[endpoint(addAdmin)]
+    fn add_admin_endpoint(&self, address: ManagedAddress) {
+        self.require_caller_is_admin();
+        self.admins().insert(address);
+    }
+
+    #[endpoint(removeAdmin)]
+    fn remove_admin_endpoint(&self, address: ManagedAddress) {
+        self.require_caller_is_admin();
+        self.admins().swap_remove(&address);
+    }
+
+    fn require_caller_is_admin(&self) {
+        let caller = self.blockchain().get_caller();
+        let is_admin = self.admins().contains(&caller);
+        let is_owner = self.blockchain().get_owner_address() == caller;
+        require!(is_admin || is_owner, "caller must be admin");
+    }
+
+    #[storage_mapper("admins")]
+    fn admins(&self) -> UnorderedSetMapper<ManagedAddress>;
 }
